1134111f5de868c8f9456e49d40427ba1ab2d965
tjcocker <tjcocker@yahoo.com>
2024-09-21 15:05:11 -0600

    home page updates

--- app/dashboard/home/page.tsx
+++ app/dashboard/home/page.tsx
@@ -15,15 +15,15 @@ const Home: NextPage = () => {
                 <h1>
                     <b>Welcome!</b>
                 </h1><br></br>
-                <p>Take a look around. Feel free to create, update, or delete invoices. (I can re-seed the data)</p>
-                <br></br>
                 <p><i>This is a project for learning Next.js and playing around with testing tools for web apps.</i></p>
+                <br></br>
+                <p>Feel free to create, update, or delete invoices. (I can re-seed the data)</p>
                 <br></br>  <br></br>     <br></br>   <Link
-            href="/dashboard"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-600 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          > <ArrowLeftIcon className="w-5 md:w-6" />
-            <span>Go to the Dashboard</span>
-          </Link>
+                    href="/dashboard"
+                    className="flex items-center gap-5 self-start rounded-lg bg-blue-600 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+                > <ArrowLeftIcon className="w-5 md:w-6" />
+                    <span>Go to the Dashboard</span>
+                </Link>
                 <div>
 
                 </div>
