7b38f6639b8a0966cf578e057b351a67b43d3f4c
tjcocker <tjcocker@yahoo.com>
2024-09-20 21:54:03 -0600

    commits

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Image from 'next/image';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-600 p-4 md:h-52">
         <MyLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
@@ -16,7 +16,7 @@ export default function Page() {
             <strong>Welcome!</strong> This is my portfolio website created with Next.js</p>
           <Link
             href="/dashboard"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+            className="flex items-center gap-5 self-start rounded-lg bg-blue-600 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
           >
             <span>Click Here</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
