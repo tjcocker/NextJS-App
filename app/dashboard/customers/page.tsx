26f8bf783f479054ffbd1f7167b6e4c73feb55d9
tjcocker <tjcocker@yahoo.com>
2024-09-21 08:02:41 -0600

    update customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,31 +1,31 @@
-export default function Page() {
-    return <p>Customers Page</p>;
-  }
+// export default function Page() {
+//     return <p>Customers Page</p>;
+//   }
 
-//   import type { Metadata } from 'next'
+import type { Metadata } from 'next'
 
-// +import { fetchFilteredCustomers } from '@/app/lib/data'
-// +import CustomersTable from '@/app/ui/customers/table'
++import { fetchFilteredCustomers } from '@/app/lib/data'
++import CustomersTable from '@/app/ui/customers/table'
 
-// export const metadata: Metadata = {
-//   title: 'Customers',
-// }
+export const metadata: Metadata = {
+  title: 'Customers',
+}
 
-// export default async function Page({
-// +  searchParams,
-// +}: {
-// + searchParams?: {
-// +    query?: string
-// +   page?: string
-// +  }
-// +}) {
-// +  const query = searchParams?.query || ''
+export default async function Page({
++  searchParams,
++}: {
++ searchParams?: {
++    query?: string
++   page?: string
++  }
++}) {
++  const query = searchParams?.query || ''
 
-// *  const customers = await fetchFilteredCustomers(query)
+*  const customers = await fetchFilteredCustomers(query)
 
-//   return (
-//     <main>
-// *      <CustomersTable customers={customers} />
-//     </main>
-//   )
-// }
+  return (
+    <main>
+*      <CustomersTable customers={customers} />
+    </main>
+  )
+}
