c72cfb5a31ab9192611ff649bfec8c89969d8462
tjcocker <tjcocker@yahoo.com>
2024-09-21 09:42:01 -0600

    mobile logo fix

--- app/ui/my-logo.tsx
+++ app/ui/my-logo.tsx
@@ -6,8 +6,8 @@ export default function MyLogo() {
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <CodeBracketSquareIcon className="size-13"/>
-      <p className="text-[35px]">Tim Cockerham</p>
+      <CodeBracketSquareIcon className="size-11"/>
+      <p className="text-[32px]">Tim Cockerham</p>
     </div>
   );
 }
