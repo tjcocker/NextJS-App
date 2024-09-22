898bc4cd0a5cf617e52814c9822024afb38c7b39
tjcocker <tjcocker@yahoo.com>
2024-09-22 08:20:51 -0600

    logo size fixes for mobile

--- app/ui/my-logo.tsx
+++ app/ui/my-logo.tsx
@@ -6,7 +6,7 @@ export default function MyLogo() {
     <div
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
-      <CodeBracketSquareIcon className="size-10"/>
+      <CodeBracketSquareIcon className="size-13"/>
       <p className="text-[30px]">Tim Cockerham</p>
     </div>
   );
