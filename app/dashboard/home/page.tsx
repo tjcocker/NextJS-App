55d6153b1d77eae514b10f05f9d4012019ac538f
tjcocker <tjcocker@yahoo.com>
2024-09-23 20:19:55 -0600

    home page updates

--- app/dashboard/home/page.tsx
+++ app/dashboard/home/page.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { ArrowLeftIcon } from '@heroicons/react/24/outline';
+import { ChartBarSquareIcon, CloudArrowDownIcon, DocumentDuplicateIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
 const Home: NextPage = () => {
@@ -17,15 +17,31 @@ const Home: NextPage = () => {
                 </h1><br></br>
                 <p><i>This is a project for learning Next.js and playing around with testing tools.</i></p>
                 <br></br>
-                <p>Feel free to create, update, or delete invoices. (I can re-seed the data)</p>
-                <br></br>  <br></br>     <br></br>   <Link
+                <p><b>Feel free to create, update, or delete invoices.</b></p><br></br>
+                <Link
                     href="/dashboard"
                     className="flex items-center gap-5 self-start rounded-lg bg-blue-600 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-                > <ArrowLeftIcon className="w-5 md:w-6" />
-                    <span>Go to the Dashboard</span>
+                > <ChartBarSquareIcon className="w-5 md:w-6" />
+                    <span>Start with the Dashboard</span>
                 </Link>
-                <div>
-
+                <br></br> <br></br><br></br>
+                <p><b>Deleted all the invoices?</b> Don't worry, I can re-seed the data.</p><br></br> <p><i>Or, you can re-seed the database yourself:</i></p><br></br>
+                <p>1. Click below:</p><br></br>
+                <Link
+                    href="/seed"
+                    className="flex items-center gap-5 self-start rounded-lg bg-blue-600 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+                > <CloudArrowDownIcon className="w-5 md:w-6" />
+                    <span>Re-seed Database</span>
+                </Link>
+                <br></br><p>2. Click the back button on your browser when you see the message, "Database Seeded Successfully".</p><br></br>
+                <p>3. Go to Invoices to see the repopulated database.</p>
+                <div><br></br>
+                <Link
+                    href="/dashboard/invoices"
+                    className="flex items-center gap-5 self-start rounded-lg bg-blue-600 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+                > <DocumentDuplicateIcon className="w-5 md:w-6" />
+                    <span>Go to Invoices</span>
+                </Link>
                 </div>
             </main>
         </div>
