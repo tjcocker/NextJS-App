5ae811441349b00188857b9bcd760752230e65c3
tjcocker <tjcocker@yahoo.com>
2024-09-23 21:56:59 -0600

    icon update

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -4,7 +4,7 @@ import {
   UserGroupIcon,
   ChartBarSquareIcon,
   DocumentDuplicateIcon,
-  HomeIcon
+  HomeModernIcon
 } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
@@ -13,7 +13,7 @@ import clsx from 'clsx';
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
 const links = [
-  { name: 'Home', href: '/dashboard/home', icon: HomeIcon },
+  { name: 'Home', href: '/dashboard/home', icon: HomeModernIcon },
   { name: 'Dashboard', href: '/dashboard', icon: ChartBarSquareIcon },
   {
     name: 'Invoices',
