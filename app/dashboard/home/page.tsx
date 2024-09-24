87d27937a8c3f96d3c1a7492617c24b719d8ceed
tjcocker <tjcocker@yahoo.com>
2024-09-23 20:23:47 -0600

    home page updates

--- app/dashboard/home/page.tsx
+++ app/dashboard/home/page.tsx
@@ -36,12 +36,12 @@ const Home: NextPage = () => {
                 <br></br><p>2. Click the back button on your browser when you see the message, "Database Seeded Successfully".</p><br></br>
                 <p>3. Go to Invoices to see the repopulated database.</p>
                 <div><br></br>
-                <Link
-                    href="/dashboard/invoices"
-                    className="flex items-center gap-5 self-start rounded-lg bg-blue-600 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-                > <DocumentDuplicateIcon className="w-5 md:w-6" />
-                    <span>Go to Invoices</span>
-                </Link>
+                    <Link
+                        href="/dashboard/invoices"
+                        className="flex items-center gap-5 self-start rounded-lg bg-blue-600 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+                    > <DocumentDuplicateIcon className="w-5 md:w-6" />
+                        <span>Go to Invoices</span>
+                    </Link>
                 </div>
             </main>
         </div>
