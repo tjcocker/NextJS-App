0eb1a26ffc2fe330d6179b7777e97a2ef8c84a78
tjcocker <tjcocker@yahoo.com>
2024-09-23 22:20:43 -0600

    added break

--- app/dashboard/home/page.tsx
+++ app/dashboard/home/page.tsx
@@ -41,7 +41,7 @@ const Home: NextPage = () => {
                         className="flex items-center gap-5 self-start rounded-lg bg-blue-600 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
                     > <DocumentDuplicateIcon className="w-5 md:w-6" />
                         <span>Go to Invoices</span><br></br>
-                    </Link>
+                    </Link><br></br>
                     <p>- Invoices will be duplicated if not deleted</p>
                 </div>
             </main>
