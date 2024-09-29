b4d7e99903201c097b5c4b05149a89348386105c
tjcocker <tjcocker@yahoo.com>
2024-09-28 19:41:10 -0600

    update home page

--- app/dashboard/contact/page.tsx
+++ app/dashboard/contact/page.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { CogIcon, BriefcaseIcon, ComputerDesktopIcon, EnvelopeIcon } from '@heroicons/react/24/outline';
+import { CogIcon, BriefcaseIcon, ComputerDesktopIcon, EnvelopeIcon, RectangleGroupIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
 const Contact: NextPage = () => {
@@ -33,7 +33,13 @@ const Contact: NextPage = () => {
                         href="https://github.com/tjcocker/NextJS-App"
                         className="flex items-center gap-5 self-start rounded-lg bg-blue-600 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
                     > <CogIcon className="w-5 md:w-6" />
-                        <span>GitHub repo for this app</span><br></br>
+                        <span>GitHub repo</span><br></br>
+                    </Link><br></br><br></br>
+                    <Link
+                        href="https://bitbucket.org/tim-cockerham/app/src/master/"
+                        className="flex items-center gap-5 self-start rounded-lg bg-blue-600 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+                    > <RectangleGroupIcon className="w-5 md:w-6" />
+                        <span>Bitbucket repo</span><br></br>
                     </Link><br></br><br></br>
                     <Link
                         href="https://tim-cockerham.com/"
