05c37e59bc1b9b8bc42ea3aebfdc8310850a880f
tjcocker <tjcocker@yahoo.com>
2024-10-15 18:21:13 -0600

    added reset function

--- app/dashboard/home/page.tsx
+++ app/dashboard/home/page.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import { ChartBarSquareIcon, ArchiveBoxArrowDownIcon, DocumentDuplicateIcon, TrashIcon } from '@heroicons/react/24/outline';
+import { ChartBarSquareIcon, ArchiveBoxArrowDownIcon, DocumentDuplicateIcon, WrenchIcon, TrashIcon, UsersIcon, ArrowPathRoundedSquareIcon, XMarkIcon, BeakerIcon, SwatchIcon, ExclamationTriangleIcon, WrenchScrewdriverIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 
 const Home: NextPage = () => {
@@ -19,44 +19,72 @@ const Home: NextPage = () => {
                 <br></br><p>It started as the basic tutorial project: &nbsp;<a
                     href="https://nextjs.org/learn/dashboard-app" ><span>https://nextjs.org/learn/dashboard-app</span>
                 </a><br></br><br></br>
-                    * I removed authentication so anyone can access the app<br></br>
-                    * Created a new &apos;Delete All&apos; function to clear the Postgres database<br></br>
-                    * Made the existing &apos;Seed Database&apos; function public so users can repopulate the database<br></br>
-                    * Added this home page, a contact page, and other cosmetic changes<br></br>
-                    * Created Selenium/Python and Cypress/JavaScript testing for database functions &amp; invoice actions</p><br></br><br></br>
+                    <a href="" className="flex items-center"
+                    ><XMarkIcon className="w-5 md:w-7" /><span>&nbsp;I removed authentication so anyone can access the app </span></a>
+
+                    <a href="" className="flex items-center"
+                    ><WrenchIcon className="w-5 md:w-7" /><span>&nbsp;Created a new &apos;Delete All&apos; function to clear the Postgres database </span></a>
+
+                    <a href="" className="flex items-center"
+                    ><UsersIcon className="w-5 md:w-7" /><span>&nbsp;Made the existing &apos;Seed Database&apos; function public so users could repopulate the database </span></a>
+
+                    <a href="" className="flex items-center"
+                    ><WrenchScrewdriverIcon className="w-5 md:w-7" /><span>&nbsp;Merged both Delete and Seed into a more user friendly &apos;Reset&apos; function </span></a>
+
+                    <a href="" className="flex items-center"
+                    ><SwatchIcon className="w-5 md:w-7" /><span>&nbsp;Added this home page, a contact page, and other cosmetic changes </span></a>
+
+                    <a href="" className="flex items-center"
+                    ><BeakerIcon className="w-5 md:w-7" /><span>&nbsp;Created Selenium and Cypress tests for database functions &amp; invoice actions </span></a></p>
+                <br></br><br></br>
                 <Link
                     href="/dashboard"
                     className="flex items-center gap-5 self-start rounded-lg bg-blue-600 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-                > <ChartBarSquareIcon className="w-5 md:w-6" />
+                > <ChartBarSquareIcon className="w-5 md:w-7" />
                     <span>Start with the Dashboard</span>
                 </Link><br></br>
+                <p>Latest invoices, amount collected, pending, etc... update as invoices are added or changed.</p>
+                <br></br>
                 <p><b>Feel free to create, edit, or delete invoices.</b></p><br></br>
                 <Link
                     href="/dashboard/invoices"
                     className="flex items-center gap-5 self-start rounded-lg bg-blue-600 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-                > <DocumentDuplicateIcon className="w-5 md:w-6" />
+                > <DocumentDuplicateIcon className="w-5 md:w-7" />
                     <span>Go to Invoices</span>
-                </Link><br></br><br></br>
-                <p><b>Deleted all the invoices?</b> </p><br></br> <p><i>You can repopulate the original data:</i></p>
+                </Link><br></br>
+                <p><b>Want to start over? Click below:</b></p>
                 <br></br>
-
                 <a
-                    href="/seed"
+                    href="/reset"
                     className="flex items-center gap-5 self-start rounded-lg bg-blue-600 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-                > <ArchiveBoxArrowDownIcon className="w-5 md:w-6" />
-                    <span>Click Here to reseed the database</span>
+                > <ArrowPathRoundedSquareIcon className="w-5 md:w-7" />
+                    <span>Click Here to reset the database</span>
                 </a>
-                <p><br></br> * Invoices will be duplicated if not deleted</p><br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <br></br>
+                <p>Separate Delete and Seed functions for testing purposes:</p><br></br>
 
-                <br></br>
-                <p><b>Want to delete all the invoices?</b></p>
-                <br></br>
+
                 <a
                     href="/delete"
                     className="flex items-center gap-5 self-start rounded-lg bg-blue-600 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-                > <TrashIcon className="w-5 md:w-6" />
+                > <TrashIcon className="w-5 md:w-7" />
                     <span>Click Here to clear the database</span>
                 </a>
+                <br></br>
+                <a
+                    href="/seed"
+                    className="flex items-center gap-5 self-start rounded-lg bg-blue-600 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+                > <ArchiveBoxArrowDownIcon className="w-5 md:w-7" />
+                    <span>Click Here to reseed the database</span>
+                </a>
+                <p><br></br>  <a href="" className="flex items-center"
+                ><ExclamationTriangleIcon className="w-5 md:w-7" /><span>&nbsp;Invoices will be duplicated if not deleted </span></a>
+                </p>
+
             </main>
         </div>
     )
