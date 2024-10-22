f801127368f39378b2c6f4ec3c17becf2df64648
tjcocker <tjcocker@yahoo.com>
2024-10-21 18:27:42 -0600

    Update table.tsx

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -77,10 +77,7 @@ export default async function InvoicesTable({
                   Status
                   
                 </th>
-                
-                <th scope="col" className="relative py-3 pl-6 pr-3 font-medium">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Edit
-                  <span className="sr-only">Edit</span>
-                </th>
+ 
               </tr>
             </thead>
             <tbody className="bg-white">
