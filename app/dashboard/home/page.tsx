c7013b93ac757b80b03d4172413af470765228a2
tjcocker <tjcocker@yahoo.com>
2024-11-29 20:18:40 -0700

    Update page.tsx

--- app/dashboard/home/page.tsx
+++ app/dashboard/home/page.tsx
@@ -7,7 +7,7 @@ import Link from 'next/link';
 
 export const metadata: Metadata = {
     title: 'Home',
-  };
+};
 
 const Home: NextPage = () => {
     return (
@@ -22,29 +22,31 @@ const Home: NextPage = () => {
                     <b data-cy="welcomeText">Welcome!</b>
                 </h1><br></br>
                 <p><i>This is a project for learning and testing a React web app. Feel free to look around and test it out.</i></p>
-                <br></br><p>It started as the basic tutorial project: &nbsp;<a
+                <br></br>
+                <p className="flex items-center"
+                ><BeakerIcon className="w-8 md:w-8" /><span>&nbsp;&nbsp;<a
+                    href="https://tim-cockerham.com/videos/cypressAppTest.mp4"><b>Cypress</b></a> e2e testing &amp; <a
+                        href="https://tim-cockerham.com/videos/appTest.mp4"><b>Selenium</b></a> tests for database functions </span></p>
+                <br></br>
+                <p>It started as the basic tutorial project: &nbsp;<a
                     href="https://nextjs.org/learn/dashboard-app" ><b><span>https://nextjs.org/learn/dashboard-app</span></b>
                 </a></p><br></br>
-                    <a className="flex items-center"
-                    ><KeyIcon className="w-8 md:w-8" /><span>&nbsp;&nbsp;I removed authentication so anyone can access the app </span></a>
+                <a className="flex items-center"
+                ><KeyIcon className="w-8 md:w-8" /><span>&nbsp;&nbsp;I removed authentication so anyone can access the app </span></a>
 
-                    <a className="flex items-center"
-                    ><WrenchIcon className="w-8 md:w-8" /><span>&nbsp;&nbsp;Created a new &apos;Delete All&apos; function to clear the Postgres database </span></a>
+                <a className="flex items-center"
+                ><WrenchIcon className="w-8 md:w-8" /><span>&nbsp;&nbsp;Created a new &apos;Delete All&apos; function to clear the Postgres database </span></a>
 
-                    <a className="flex items-center"
-                    ><UsersIcon className="w-8 md:w-8" /><span>&nbsp;&nbsp;Made the existing &apos;Seed Database&apos; function public so users could repopulate the database </span></a>
+                <a className="flex items-center"
+                ><UsersIcon className="w-8 md:w-8" /><span>&nbsp;&nbsp;Made the existing &apos;Seed Database&apos; function public so users could repopulate the database </span></a>
 
-                    <a className="flex items-center"
-                    ><WrenchScrewdriverIcon className="w-8 md:w-8" /><span>&nbsp;&nbsp;Merged both Delete and Seed into a more user friendly &apos;Reset&apos; function </span></a>
+                <a className="flex items-center"
+                ><WrenchScrewdriverIcon className="w-8 md:w-8" /><span>&nbsp;&nbsp;Merged both Delete and Seed into a more user friendly &apos;Reset&apos; function </span></a>
 
-                    <a className="flex items-center"
-                    ><SwatchIcon className="w-8 md:w-8" /><span>&nbsp;&nbsp;Added this home page, a contact page, and other cosmetic changes </span></a>
-                    <br></br>
-                    <p className="flex items-center"
-                    ><BeakerIcon className="w-8 md:w-8" /><span>&nbsp;&nbsp;<a
-                    href="https://tim-cockerham.com/videos/cypressAppTest.mp4"><b>Cypress</b></a> e2e testing &amp; <a
-                    href="https://tim-cockerham.com/videos/appTest.mp4"><b>Selenium</b></a> tests for database functions </span></p>
-                <br></br><br></br>
+                <a className="flex items-center"
+                ><SwatchIcon className="w-8 md:w-8" /><span>&nbsp;&nbsp;Added this home page, a contact page, and other cosmetic changes </span></a>
+                <br></br>
+                
                 <Link
                     href="/dashboard"
                     className="flex items-center gap-5 self-start rounded-lg bg-blue-600 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
@@ -68,9 +70,6 @@ const Home: NextPage = () => {
                 > <ArrowPathRoundedSquareIcon className="w-5 md:w-7" />
                     <span>Click Here to reset the database</span>
                 </a>
-                <br></br>
-                <br></br>
-                <br></br>
                 <br></br>
                 <br></br>
                 <p>Separate Delete and Seed functions for testing purposes:</p><br></br>
