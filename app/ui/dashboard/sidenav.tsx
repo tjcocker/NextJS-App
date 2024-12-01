9cfcbb815ef561263b6147b92875a3e0de2ba723
tjcocker <tjcocker@yahoo.com>
2024-11-30 18:46:01 -0700

    Update sidenav.tsx

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -6,7 +6,7 @@ import MyLogo from '@/app/ui/my-logo';
 
 export default function SideNav() {
   return (
-    <div data-cy="sidenav" className="flex h-full flex-col px-3 py-4 md:px-2">
+    <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
         className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
         href="/"
