41472831ff54643a8f3a849d644b807ddce06faf
tjcocker <tjcocker@yahoo.com>
2024-11-30 18:50:12 -0700

    Update page.tsx

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -6,7 +6,7 @@ export default async function Page() {
   const customers = await fetchCustomers();
  
   return (
-    <h1><main>
+    <main>
       <Breadcrumbs
         breadcrumbs={[
           { label: 'Invoices', href: '/dashboard/invoices' },
@@ -18,6 +18,6 @@ export default async function Page() {
         ]}
       />
       <Form customers={customers} />
-    </main></h1>
+    </main>
   );
 }
